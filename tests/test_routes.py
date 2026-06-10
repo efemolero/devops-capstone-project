453587ea1bae7c9edc4ad4a01c986e6ac66bcb3b
efemolero <fati_dm@yahoo.es>
2026-06-10 14:24:57 -0400

    delete an account

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -182,3 +182,14 @@ class TestAccountService(TestCase):
         )
 
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_account(self):
+        """It should Delete an Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.delete(f"{BASE_URL}/{account.id}")
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+    
+    def test_delete_account_not_found(self):
+        """It should not Delete an non-existing Account"""
+        resp = self.client.delete(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)

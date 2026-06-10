8f166167a1bbf7088de18e119fdacda1932e19bc
efemolero <fati_dm@yahoo.es>
2026-06-10 13:25:05 -0400

    added list accounts

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -124,3 +124,18 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     # ADD YOUR TEST CASES HERE ...
+    def test_get_account_list(self):
+        """It should list Accounts"""
+        self._create_accounts(5)
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)
+
+    def test_get_account_list(self):
+        """It should retrieve empty list of Accounts"""
+        self._create_accounts(0)
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 0)
